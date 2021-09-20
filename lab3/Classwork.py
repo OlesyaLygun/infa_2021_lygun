6d055c47ca31c39563f64e93a147d13994c823a9
Olesya <lygun.oa@phystech.edu>
2021-09-20 18:22:47 +0300

    Drew evil face

---
+++ lab3/Classwork.py
@@ -0,0 +1,5 @@
+from graph import *
+windowSize(800, 550)
+brushColor(255, 174, 126)
+circle(0, 0, 1000)
+run()
---
+++ lab3/Project.py
@@ -0,0 +1,18 @@
+from graph import *
+
+
+from graph import *
+penColor("black")
+brushColor("yellow")
+circle(250, 250, 100)
+brushColor("red")
+circle(200, 230, 20)
+circle(280, 230, 15)
+brushColor("black")
+circle(200, 230, 7)
+circle(280, 230, 6)
+penSize(10)
+line(150, 180, 230, 230)
+line(250, 230, 350, 180)
+rectangle(200, 300, 290, 310)
+run()
