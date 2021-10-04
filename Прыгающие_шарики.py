65b8c38a2cdd45e375ff61cb63ff0f1f27a5ad3a
OlesyaLygun <91071039+OlesyaLygun@users.noreply.github.com>
2021-10-04 18:29:33 +0300

    Add files via upload
    Шарики двигаются и ускоряются при нажатии

---
+++ Прыгающие_шарики.py
@@ -0,0 +1,81 @@
+from tkinter import *
+from random import randrange as rnd, choice
+import time
+
+root = Tk()
+root.geometry('800x600')
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=1)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+class Ball(object):
+    # Класс цветных шариков.
+        # X, y - координаты, r - радиус
+        # Color - цвет
+        #vel_x, vel_y - проекции скорости на x и на y соответственно
+
+        def __init__(self, x, y, r, color, vel_x=3, vel_y=3):
+            self.color = color
+            self.x = x
+            self.y = y
+            self.r = r
+            self.vel_x = vel_x
+            self.vel_y = vel_y
+
+
+balls = []
+for i in range(10):
+    balls.append(Ball(rnd(100, 700), rnd(100, 500), rnd(30, 50), choice(colors)))
+
+
+def new_ball():
+    global Ball1
+    # Создает новые шары раз в 1 секунду, вызывая саму себя.
+    # x, y - координаты (случайные в промежутке 100-700, 100-500
+    # r - радиус
+    canv.delete(ALL)
+    for i in range(10):
+        canv.create_oval(balls[i].x - balls[i].r, balls[i].y - balls[i].r, balls[i].x + balls[i].r, balls[i].y + balls[i].r, fill=balls[i].color, width=0)
+    root.after(1000, balls_movements)
+
+def balls_movements():
+    #Передвигает каждый из 10 шаров на его vel_y по вертикали, vel_x - горизонтали
+    for i in range(10):
+        balls[i].y += balls[i].vel_y
+        balls[i].x += balls[i].vel_x
+        if balls[i].x > 800 - balls[i].r or balls[i].x < 0 + balls[i].r:
+            balls[i].vel_x *= -1
+        if balls[i].y > 600 - balls[i].r or balls[i].y < 0 + balls[i].r:
+            balls[i].vel_y *= -1
+    canv.delete(ALL)
+    for i in range(10):
+        canv.create_oval(balls[i].x - balls[i].r, balls[i].y - balls[i].r, balls[i].x + balls[i].r, balls[i].y + balls[i].r, fill=balls[i].color,
+                     width=0)
+    #просто пишем строку, чтобы она не исчезала после каждого del(ALL)
+    canv.create_text(100, 100, text='your score: ' + str(point),
+                              justify=CENTER, font="Verdana 14")
+    root.after(10, balls_movements)
+
+def click(event):
+    #Обрабатывает клик. Если игрок попадает в шар, то количество очков увеличивается + скорость нажатого шарика увеличивается на количество
+    #очков. После скорость равное 20, шар лопается
+    global point
+    scores = canv.create_text(100, 100, text="",
+                      justify=CENTER, font="Verdana 14")
+    for i in range(10):
+        if ((event.x - balls[i].x)**2 + (event.y - balls[i].y)** 2) ** 0.5 <= balls[i].r:
+            canv.create_rectangle(35, 90, 170, 110, fill='white', outline='white')
+            point += 1
+            scores = canv.create_text(100, 100, text='your score: ' + str(point),
+                      justify=CENTER, font="Verdana 14")
+            balls[i].vel_x += point
+            balls[i].vel_y += point
+            if balls[i].vel_x > 20:
+                balls[i].r = 0
+
+
+
+point = 0
+new_ball()
+canv.bind('<Button-1>', click)
+mainloop()
